31477397727dd9d06028188d38b0f1ec2f55cbea
Neo <echiang73@gmail.com>
2019-02-15 13:39:40 -0700

    got code working with some css, still need to clean up code and fix repeat letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 // Global Codes - Setup
 
 //Create an array of words (NBA teams)
-var wordList = ["HAWKS", "CELTICS", "NET", "HORNETS", "BULLS", "CAVALIERS", "MAVERICKS", "NUGGETS", "PISTONS", "WARRIORS", "ROCKETS", "PACERS", "CLIPPERS", "LAKERS", "GRIZZLIES", "HEAT", "BUCKS", "TIMBERWOLVES", "PELICANS", "KNICKS", "THUNDER", "MAGIC", "SIXERS", "SUNS", "BLAZERS", "KINGS", "SPURS", "RAPTORS", "JAZZ", "WIZARDS"];
+var wordList = ["HAWKS", "CELTICS", "NETS", "HORNETS", "BULLS", "CAVALIERS", "MAVERICKS", "NUGGETS", "PISTONS", "WARRIORS", "ROCKETS", "PACERS", "CLIPPERS", "LAKERS", "GRIZZLIES", "HEAT", "BUCKS", "TIMBERWOLVES", "PELICANS", "KNICKS", "THUNDER", "MAGIC", "SIXERS", "SUNS", "BLAZERS", "KINGS", "SPURS", "RAPTORS", "JAZZ", "WIZARDS"];
 
 // Generate a random number to pick a word from the array
-var choice = (Math.floor(Math.random()*wordList.length));
+var choice = (Math.floor(Math.random() * wordList.length));
 var answer = wordList[choice];
 var answerLength = answer.length;
 console.log(choice);
@@ -16,6 +16,7 @@ var win = answerLength;
 
 // Set how many guesses the user is allowed (i.e. 5 more than length of answer)
 var attemptsLeft = answerLength + 5;
+console.log(attemptsLeft);
 
 // Display _ for each letter in the word to be guessed
 var display = [answerLength];
@@ -24,57 +25,79 @@ var userLetter = "";
 
 // Break the string answer apart and store it as an array of letters
 var letters = answer.split("");
+// console.log(letters);
 
 // Local Codes - The Game
 
-//Click any key to start??
-// document.onkeyup = function(start);
+//Click any key to start
+// document.onkeyup = function(start); {
+
+// }
 
 // Load function
+// $(document).ready(function() {
+//     $("#game").on("click", function() {
+//       alert("I've been clicked!");
+//     });
+//   });
 
-    $("submit").onclick = submit;
-    var setup = function() {
-        for (var i=0; i < answer.length; i++){
-            display[i] = "_ ";
-            outplut = output + display[i];
-        }
-        document.getElementById("game").innerHTML = output;
-        output ="";
+
+
+// setup display letters to be guessed in HTML
+var setup = function () {
+    for (var i = 0; i < answer.length; i++) {
+        display[i] = "_ ";
+        output = output + display[i];
+        // console.log(output);
     }
-
-    var submit = function(){
-        output = "";
-        userLetter=$("letter").value;
-        $("letter").value = "";
-    
-        for (var c=0; c < answer.length; c++) {
-            alert(letter[c]);
-            if (userLetter.toUpperCase() == letters[c]) {
-                display[c] = userLetter.toUpperCase();
-                win--;
-            }
-            output = output + display[c] + "";
-        }
-    
-        document.getElementById("game").innerHTML = output;
-        output = "";
-        attemptsLeft--;
-    
-        if (win < 1) {
-            document.getElementById("guesses").innerHTML = "Hooray, you Win!";
-        }
-        else if (attempsLeft < 1) {
-            document.getElementById("guesses").innerHTML = "Sorry, you Lose!";
-        }
-        else {
-            document.getElementById("guesses").innerHTML = "You have " + attemptsLeft + "guesses left!";
-        }
-    }
-
-window.onload = function() {
-    setup();
-
+    document.getElementById("game").innerHTML = output;
+    output = "";
+    document.getElementById("guesses").innerHTML = "You have " + attemptsLeft + " guesses to solve this!";
 }
+
+var submit = function () {
+    // alert("it works when clicked!")
+    output = "";
+    // userLetter = $("#letter").value;
+    userLetter = document.getElementById("guessField").value;
+    console.log(userLetter);
+    // $("letter").value = "";
+    document.getElementById("guessField").value = "";
+
+    for (var c = 0; c < answer.length; c++) {
+        // alert(letters[c]);
+        if (userLetter.toUpperCase() == letters[c]) {
+            display[c] = userLetter.toUpperCase();
+            win--;
+        }
+        output = output + display[c] + "";
+    }
+
+    document.getElementById("game").innerHTML = output;
+    output = "";
+    attemptsLeft--;
+    console.log(attemptsLeft);
+
+    if (win < 1) {
+        document.getElementById("guesses").innerHTML = "Hooray, you Win!";
+    }
+    else if (attemptsLeft < 1) {
+        document.getElementById("guesses").innerHTML = "Sorry, you Lose!";
+    }
+    else {
+        document.getElementById("guesses").innerHTML = "You have " + attemptsLeft + " guesses left!";
+    }
+}
+$(document).ready(function() {
+    setup();
+});
+// window.onload = function () {
+//     setup();
+//     // $("letterSubmit").onclick = submit;
+    
+// }
+
+
 
 
 
@@ -84,6 +107,4 @@ window.onload = function() {
 // Compare user Letter to the answer:  
 //If the letter matches a letter in the answer: replace the _ with a letter in the display
 // Subtract one from the number of letters that need to be guessed to win
-
-
 
